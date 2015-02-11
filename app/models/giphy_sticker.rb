1595f1b696a695678f4965e749f21484861d2923
aaronzdavis <aaronzdavis@gmail.com>
2015-02-11 10:14:44 -0800

    remove limit

--- app/models/giphy_sticker.rb
+++ app/models/giphy_sticker.rb
@@ -1,21 +1,20 @@
 class GiphySticker
 
-  def self.search(search, limit)
+  def self.search(search)
     offset      = rand(0..100)
     api_key     = 'dc6zaTOxFJmzC'
     sticker_url = 'http://api.giphy.com/v1/stickers/search'
     response    = HTTParty.get sticker_url,
                     query: {
                             q: search,
-                            api_key: api_key,
                             offset: offset,
-                            limit: limit
+                            api_key: api_key
                            }
     Hashie::Mash.new response
   end
 
   def self.cat_gif
-    self.search("cats", 1).data.sample.images.original.url
+    self.search("cats").data.sample.images.original.url
   end
 
 end
