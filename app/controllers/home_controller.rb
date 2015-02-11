8370c68be927aeff6401c2e4c8b903f75151ce60
aaronzdavis <aaronzdavis@gmail.com>
2015-02-11 15:07:38 -0800

    add permalinks and click to refresh

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,10 @@
 class HomeController < ApplicationController
   def index
-    params[:q] ||= "cats"
-    @gif = GiphySticker.rand_gif(params[:q])
+    if params[:g]
+      @gif = params[:g]
+    else
+      params[:q] ||= "cats"
+      @gif = GiphySticker.rand_gif(params[:q])
+    end
   end
 end
