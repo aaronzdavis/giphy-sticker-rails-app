080c3f2f2b70f5c748300ac312bde1ae2254bcc5
aaronzdavis <aaronzdavis@gmail.com>
2015-11-12 13:42:00 -0800

    Add users to app

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   root 'home#index'
+
+  # Users
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+  # Sessions
+  get '/login'  => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  # Dashboard
+  get '/dashboard' => 'dashboard#index'
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20151112210333) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
 
 end
